8975dcd625c65284b0944f1b61e16f448eee4699
Vitor Filardi <rabelo51@gmail.com>
2020-04-26 18:07:10 -0300

    Add fetching for historical prices

--- screens/historical_prices.js
+++ screens/historical_prices.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Container, Item, Input, Content, Grid, Row} from 'native-base';
+import React, {useState} from 'react';
+import {Container, Item, Input, Content, Grid, Row, Text} from 'native-base';
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
@@ -10,14 +10,38 @@ const styles = StyleSheet.create({
 });
 
 function HistoricalPrices({navigation}) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState('');
+  //   const state = {
+  //     data: '',
+  //     loading: false,
+  //   };
+  var getHistoricalPrices = () => {
+    fetch(
+      'https://api.stockdio.com/data/financial/info/v1/GetCompanyInfo?app-key=C58D15F86B0B48F2A393FA198819B881&symbol=AAPL',
+    )
+      .then(response => response.json())
+      .then(resposeJson => {
+        setData(resposeJson.data.symbol);
+        setLoading(false);
+      });
+  };
   return (
     <Container>
       <Content>
         <Grid>
           <Row style={styles.searchRow}>
             <Item rounded>
-              <Input placeholder="ticker da ação. Ex: PETR3" />
+              <Input
+                onChangeText={() => {
+                  getHistoricalPrices();
+                }}
+                placeholder="ticker da ação. Ex: PETR3"
+              />
             </Item>
+          </Row>
+          <Row>
+            <Text>{data}</Text>
           </Row>
         </Grid>
       </Content>
