983bcd8348abf278bb7295829378b7a56f76f028
Vitor Filardi <rabelo51@gmail.com>
2020-04-26 15:06:30 -0300

    Add Native Base library

--- screens/company_info.js
+++ screens/company_info.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Container, Text} from 'native-base';
 
 function CompanyInfo({navigation}) {
   return (
-    <View>
+    <Container>
       <Text>Informações da Empresa</Text>
-    </View>
+    </Container>
   );
 }
 
