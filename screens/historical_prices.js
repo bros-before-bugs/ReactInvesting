0aba1651df6a502a5c015b54cce18798fa10131b
Vitor Filardi <rabelo51@gmail.com>
2020-05-09 17:16:40 -0300

    Adição de pesquisa de preços históricos através do ticker

--- screens/historical_prices.js
+++ screens/historical_prices.js
@@ -11,24 +11,13 @@ const styles = StyleSheet.create({
 });
 
 function HistoricalPrices({navigation}) {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState('');
-  const iframe =
-    '<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&symbol=AAPL&dividends=true&splits=true&palette=Financial-Light&width=400px"></iframe>';
-  const uriIFrame = '';
-  //   const state = {
-  //     data: '',
-  //     loading: false,
-  //   };
-  var getHistoricalPrices = () => {
-    fetch(
-      'https://api.stockdio.com/data/financial/info/v1/GetCompanyInfo?app-key=C58D15F86B0B48F2A393FA198819B881&symbol=AAPL',
-    )
-      .then(response => response.json())
-      .then(resposeJson => {
-        setData(resposeJson.data.symbol);
-        setLoading(false);
-      });
+    
+  const [iframe, setIframe] = useState('<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&stockExchange=Bovespa&symbol=PETR3&dividends=true&splits=true&palette=Financial-Light&width=400px"></iframe>');
+  
+  var getHistoricalPrices = (ticker) => {
+    if(ticker.length < 5)
+        return;
+    setIframe(`<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&stockExchange=Bovespa&symbol=${ticker}&dividends=true&splits=true&palette=Financial-Light&width=400px"></iframe>`)
   };
   return (
     <Container>
@@ -37,24 +26,20 @@ function HistoricalPrices({navigation}) {
                 <Row style={styles.searchRow}>
                 <Item rounded>
                     <Input
-                    onChangeText={() => {
-                        getHistoricalPrices();
+                    onChangeText={(ticker) => {
+                        getHistoricalPrices(ticker);
                     }}
                     placeholder="ticker da ação. Ex: PETR4"
                     />
                 </Item>
                 </Row>
-                {/* <Row>
-                <Text>{data}</Text>
-                </Row> */}
                 <Row style={{}}>
                 <WebView
                     scalesPageToFit={false}
                     bounces={false}
                     javaScriptEnabled
                     style={{height: 420, width: 950, marginTop: 0, paddingTop: 0}}
-                    source={{
-                    html: `${iframe}`,
+                    source={{ html: iframe,
                     }}
                     automaticallyAdjustContentInsets={true}
                 />
