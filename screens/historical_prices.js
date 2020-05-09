5496c96678f86cf149d2952704d0f7ee22dd31f4
Vitor Filardi <rabelo51@gmail.com>
2020-05-09 15:25:50 -0300

    Adição de react-native-webview + stockdio chart

--- screens/historical_prices.js
+++ screens/historical_prices.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Container, Item, Input, Content, Grid, Row, Text} from 'native-base';
 import {StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
 
 const styles = StyleSheet.create({
   searchRow: {
@@ -12,6 +13,9 @@ const styles = StyleSheet.create({
 function HistoricalPrices({navigation}) {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState('');
+  const iframe =
+    '<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&symbol=AAPL&dividends=true&splits=true&palette=Financial-Light&width=400px"></iframe>';
+  const uriIFrame = '';
   //   const state = {
   //     data: '',
   //     loading: false,
@@ -28,23 +32,35 @@ function HistoricalPrices({navigation}) {
   };
   return (
     <Container>
-      <Content>
-        <Grid>
-          <Row style={styles.searchRow}>
-            <Item rounded>
-              <Input
-                onChangeText={() => {
-                  getHistoricalPrices();
-                }}
-                placeholder="ticker da ação. Ex: PETR3"
-              />
-            </Item>
-          </Row>
-          <Row>
-            <Text>{data}</Text>
-          </Row>
-        </Grid>
-      </Content>
+        <Content>
+            <Grid>
+                <Row style={styles.searchRow}>
+                <Item rounded>
+                    <Input
+                    onChangeText={() => {
+                        getHistoricalPrices();
+                    }}
+                    placeholder="ticker da ação. Ex: PETR4"
+                    />
+                </Item>
+                </Row>
+                {/* <Row>
+                <Text>{data}</Text>
+                </Row> */}
+                <Row style={{}}>
+                <WebView
+                    scalesPageToFit={false}
+                    bounces={false}
+                    javaScriptEnabled
+                    style={{height: 420, width: 950, marginTop: 0, paddingTop: 0}}
+                    source={{
+                    html: `${iframe}`,
+                    }}
+                    automaticallyAdjustContentInsets={true}
+                />
+                </Row>
+            </Grid>
+        </Content>
     </Container>
   );
 }
