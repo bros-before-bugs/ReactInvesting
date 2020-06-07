38a0d9be6076f9d2ac87465a7c991b27eef53136
Vitor Filardi <rabelo51@gmail.com>
2020-06-07 18:35:56 -0300

    Adição de requisições de iframe baseado nas ações escolhidas

--- screens/stock_price_comparison.js
+++ screens/stock_price_comparison.js
@@ -1,65 +1,65 @@
-import React, {useState} from 'react';
-import {Container, Item, Input, Content, Grid, Row, Text, Button, Col, Icon, Toast} from 'native-base';
-import {StyleSheet} from 'react-native';
-import {WebView} from 'react-native-webview';
+import React, { useState } from 'react';
+import { Container, Item, Input, Content, Grid, Row, Text, Button, Col, Icon, Toast } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const styles = StyleSheet.create({
-  searchRow: {
-    alignItems: 'center',
-    padding: 30,
-  },
-  // AddTicker Feature
-  addTickerButton: {
-    width: 52,
-    height: 52, 
-    borderRadius: 26, 
-    borderWidth: 3,
-    borderColor: '#65497A',
-    backgroundColor: '#9B7DE3',
-    alignContent: "center",
-    justifyContent: "center"
-  },
-  addTickerText: {
-    fontSize: 15
-  },
-  AddTickerIcon: {
-      fontSize: 15,
-  },
-  // TickerSearched Feature
-  TickerSearchedRow: {
-    justifyContent: 'space-evenly'
-  },
-  TickerSearchedButton: {
-      height: 30
-  },
-  TickerSearchedIcon: {
-      fontSize: 15
-  },
-  Toaster: {
-      backgroundColor: 'red'
-  }
+    searchRow: {
+        alignItems: 'center',
+        padding: 30,
+    },
+    // AddTicker Feature
+    addTickerButton: {
+        width: 52,
+        height: 52,
+        borderRadius: 26,
+        borderWidth: 3,
+        borderColor: '#65497A',
+        backgroundColor: '#9B7DE3',
+        alignContent: "center",
+        justifyContent: "center"
+    },
+    addTickerText: {
+        fontSize: 15
+    },
+    AddTickerIcon: {
+        fontSize: 15,
+    },
+    // TickerSearched Feature
+    TickerSearchedRow: {
+        justifyContent: 'space-evenly'
+    },
+    TickerSearchedButton: {
+        height: 30
+    },
+    TickerSearchedIcon: {
+        fontSize: 15
+    },
+    Toaster: {
+        backgroundColor: 'red'
+    }
 });
 
-function StockPriceComparison({navigation}) {
-    
-    const [iframe, setIframe] = useState('<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&stockExchange=Bovespa&symbol=PETR3&addVolume=false&dividends=true&showUserMenu=false&culture=Portuguese-Brasil=false&splits=true&palette=Financial-Light&width=400px"></iframe>');
+const colors = ['0009F0', 'F05D18', 'BD00EB']; // blue, orange, purple
+
+function StockPriceComparison({ navigation }) {
+
+    const [iframe, setIframe] = useState('');
     const [tickerSearchedList, setTickerSearchedList] = useState([]);
     const [tickerSearchedObject, setTickerSearchedObject] = useState({})
     const [typedTicker, setTypedTicker] = useState('');
 
-    let getHistoricalPrices = (ticker) => {
-        if(ticker.length < 5)
-            return;
-        setIframe(`<iframe frameBorder="0" scrolling="no" width="400" height="420" src="https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=C58D15F86B0B48F2A393FA198819B881&stockExchange=Bovespa&symbol=${ticker.toUpperCase()}&addVolume=false&dividends=true&showUserMenu=false&culture=Portuguese-Brasil=false&splits=true&palette=Financial-Light&width=400px"></iframe>`)
-    };
-
-    let buildTickerSearchedButton = (ticker) =>{
-        let tickerSearchedButton = <Button onPress={() => removeSearchedTicker(ticker)} rounded style={styles.TickerSearchedButton}><Text style={styles.TickerSearchedText}>{ticker}</Text><Icon style={styles.TickerSearchedIcon} type="FontAwesome" name="remove" /></Button>;
+    let buildTickerSearchedButton = (ticker) => {
+        let tickerSearchedButton = 
+            <Button onPress={() => removeSearchedTicker(ticker)} rounded style={{height: 30, backgroundColor: `#${colors[Object.keys(tickerSearchedObject).length]}`}}>
+                <Text style={styles.TickerSearchedText}>{ticker}</Text>
+                <Icon style={styles.TickerSearchedIcon} type="FontAwesome" name="remove" />
+            </Button>;
         return tickerSearchedButton;
     };
 
     let addSearchedTicker = () => {
-        if(typedTicker.length < 5){
+        if (typedTicker.length < 5) {
             Toast.show({
                 text: 'é preciso inserir um ticker válido',
                 buttonText: 'Ok',
@@ -68,13 +68,15 @@ function StockPriceComparison({navigation}) {
                 style: styles.Toaster
             })
         }
-        else 
-        {
+        else {
+            console.log(`Object.keys(tickerSearchedObject) ${Object.keys(tickerSearchedObject).length}`);
+
             tickerSearchedObject[typedTicker] = buildTickerSearchedButton(typedTicker);
             setTickerSearchedObject(tickerSearchedObject);
             
             setTickerSearchedList(Object.values(tickerSearchedObject))
             setTypedTicker('');
+            showStockComparison();
         }
     }
 
@@ -83,43 +85,56 @@ function StockPriceComparison({navigation}) {
         setTickerSearchedObject(tickerSearchedObject);
 
         setTickerSearchedList(Object.values(tickerSearchedObject));
-        console.log("removeSearchedTicker" + ticker);
+        // console.log("removeSearchedTicker" + ticker);
+        showStockComparison();
     }
 
     let saveTipedTicker = (text) => {
         setTypedTicker(text);
     }
 
-  return (
-    <Container>
-        <Content>
-            <Grid>
-                <Row style={styles.searchRow}>
-                    <Col style={{ width: '70%', padding: 10 }}>
-                        <Item rounded>
-                            <Input
-                            onChangeText={(ticker) => {
-                                saveTipedTicker(ticker);
-                            }}
-                            placeholder="ticker da ação. Ex: PETR4"
-                            value={typedTicker}
-                            />
-                        </Item>
-                    </Col>
-                    <Col style={{ width: '30%', padding: 10 }}>
-                        <Button 
-                            style={styles.addTickerButton}
-                            onPress={addSearchedTicker}
+    let showStockComparison = () => {
+        const tickers = Object.keys(tickerSearchedObject);
+        let symbol = tickers[0];
+        let comparisonTickers = tickers.splice(1).join(';');
+        // console.log("========================================================")
+        // console.log(`Object.keys(tickerSearchedObject) ${Object.keys(tickerSearchedObject).length}`);
+        // console.log(`tickerSearchedObject keys ${Object.keys(tickerSearchedObject).join(' ')}`);
+        if(Object.values(tickerSearchedObject).length > 1){
+            setIframe(`<iframe frameBorder='0' scrolling='no' width='400' height='420' src='https://api.stockdio.com/visualization/financial/charts/v1/ComparePrices?app-key=C58D15F86B0B48F2A393FA198819B881&stockExchange=Bovespa&symbol=${symbol.toUpperCase()}&compare=${comparisonTickers.toUpperCase()}&addVolume=false&culture=Portuguese-Brasil&palette=Financial-Light&baseColor=0009f0&compare1Color=f05d18&compare2Color=bd00eb&width=400px'></iframe>`)
+        }
+    }
+
+    return (
+        <Container>
+            <Content>
+                <Grid>
+                    <Row style={styles.searchRow}>
+                        <Col style={{ width: '70%', padding: 10 }}>
+                            <Item rounded>
+                                <Input
+                                    onChangeText={(ticker) => {
+                                        saveTipedTicker(ticker);
+                                    }}
+                                    placeholder="ticker da ação. Ex: PETR4"
+                                    value={typedTicker}
+                                />
+                            </Item>
+                        </Col>
+                        <Col style={{ width: '30%', padding: 10 }}>
+                            <Button
+                                style={styles.addTickerButton}
+                                onPress={addSearchedTicker}
                             >
-                            <Icon
-                                style={styles.AddTickerIcon} 
-                                type="FontAwesome" 
-                                name="plus" />
-                        </Button>
-                    </Col>
-                </Row>
-                <Row style={styles.TickerSearchedRow}>
-                    {/* <Button rounded style={styles.TickerSearchedButton}>
+                                <Icon
+                                    style={styles.AddTickerIcon}
+                                    type="FontAwesome"
+                                    name="plus" />
+                            </Button>
+                        </Col>
+                    </Row>
+                    <Row style={styles.TickerSearchedRow}>
+                        {/* <Button rounded style={styles.TickerSearchedButton}>
                         <Text style={styles.TickerSearchedText}>PETR4</Text>
                         <Icon style={styles.TickerSearchedIcon} type="FontAwesome" name="remove" />
                     </Button>
@@ -131,23 +146,24 @@ function StockPriceComparison({navigation}) {
                         <Text style={styles.TickerSearchedText}>PETR4</Text>
                         <Icon style={styles.TickerSearchedIcon} type="FontAwesome" name="remove" />
                     </Button> */}
-                    {tickerSearchedList}
-                </Row>
-                <Row style={{}}>
-                <WebView
-                    scalesPageToFit={false}
-                    bounces={false}
-                    javaScriptEnabled
-                    style={{height: 420, width: 950, marginTop: 0, paddingTop: 0}}
-                    source={{ html: iframe,
-                    }}
-                    automaticallyAdjustContentInsets={true}
-                />
-                </Row>
-            </Grid>
-        </Content>
-    </Container>
-  );
+                        {Object.values(tickerSearchedObject)}
+                    </Row>
+                    <Row style={{}}>
+                        <WebView
+                            scalesPageToFit={false}
+                            bounces={false}
+                            javaScriptEnabled
+                            style={{ height: 420, width: 950, marginTop: 0, paddingTop: 0 }}
+                            source={{
+                                html: iframe,
+                            }}
+                            automaticallyAdjustContentInsets={true}
+                        />
+                    </Row>
+                </Grid>
+            </Content>
+        </Container>
+    );
 }
 
 export default StockPriceComparison;
